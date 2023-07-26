58899f334aaa19e42dfa975cb84ef2b486c05b18
khaledCSE <khaledcse10@gmail.com>
2023-07-26 21:04:46 +0600

    Form UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddEditEmployeeComponent } from './add-edit-employee/add-edit-employee.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-crud';
+  constructor(private dialog: MatDialog) { }
+
+  openAddEditForm() {
+    this.dialog.open(AddEditEmployeeComponent)
+  }
 }
